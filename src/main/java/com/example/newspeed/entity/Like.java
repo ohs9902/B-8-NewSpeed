d5f9c2989d62c0366ee27b20ca1a338c31186458
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-06-07 19:52:53 +0900

    Content Like 기능 구현

---
+++ src/main/java/com/example/newspeed/entity/Like.java
@@ -0,0 +1,36 @@
+package com.example.newspeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "content_id")
+    private Content content;
+
+
+//    @ManyToOne
+//    @JoinColumn(name = "comment_id")
+//    private Comment comment;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+}
---
+++ src/main/java/com/example/newspeed/excetion/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.newspeed.excetion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+}
