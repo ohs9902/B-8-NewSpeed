8bc396504cb3c86809a493fbcc83c1e5b795f25f
ohyeongseok <dhgudtjr6635@gmail.com>
2024-06-07 18:01:58 +0900

    회원탈퇴 기능 구현 (회원탈퇴시 쿠키삭제는 미구현 )

--- src/main/java/com/example/newspeed/repository/UserRepository.java
+++ src/main/java/com/example/newspeed/repository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByUserName(String username);
+    Optional<User> findByUserIdAndPassword(String username,String password);
 }
--- src/main/java/com/example/newspeed/service/UserService.java
+++ src/main/java/com/example/newspeed/service/UserService.java
@@ -1,22 +1,31 @@
 package com.example.newspeed.service;
 
+import com.example.newspeed.dto.LoginRequestDto;
 import com.example.newspeed.dto.SignUpRequestDto;
 import com.example.newspeed.entity.User;
+import com.example.newspeed.jwt.LogoutFilter;
 import com.example.newspeed.repository.UserRepository;
+import jakarta.servlet.ServletException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-
+    private final LogoutFilter logoutFilter;
     private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, LogoutFilter logoutFilter, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.logoutFilter = logoutFilter;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -38,6 +47,26 @@ public class UserService {
 
         User user = new User(userId,password,name,email,intro,status);
         userRepository.save(user);
+    }
+    @Transactional
+    public void withdrawal(LoginRequestDto loginRequestDto) throws ServletException, IOException { //회원삭제에 필요한 필드와 로그인에 필요한 필드가 동일함으로 dto재사용
+        System.out.println("회원탈퇴 서비스 진입");
+        String password = passwordEncoder.encode(loginRequestDto.getPassword());
+       Optional<User> optionalUser = userRepository.findByUserId(loginRequestDto.getUserId());
+       if (optionalUser.isPresent()){
+           User user = optionalUser.get();
+           if(password.equals(password)){
+               user.setStatus("탈퇴");
+               userRepository.save(user);
+               SecurityContextHolder.clearContext(); // 현재 사용자의 인증 정보를 제거
+           }else{
+               throw new BadCredentialsException("비밀번호가 일치하지 않습니다");
+           }
+       }else{
+           throw new UsernameNotFoundException("사용자를 찾을 수 없거나 비밀번호가 일치하지 않습니다.");
+       }
+
+
     }
 
 
