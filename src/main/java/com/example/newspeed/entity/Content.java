4509185bc53b6c07167c7e164051629867d838f1
LSH <k01094060462@gmail.com>
2024-06-07 11:08:08 +0900

    content변수수정

--- src/main/java/com/example/newspeed/entity/Content.java
+++ src/main/java/com/example/newspeed/entity/Content.java
@@ -14,8 +14,9 @@ public class Content {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long authorId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Lob
     private String content;
