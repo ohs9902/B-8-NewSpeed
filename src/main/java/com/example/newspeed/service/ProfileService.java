d566cac62bd240ba0b8c27ca66ccdebab4aa2621
haeuni00 <leejw6311@naver.com>
2024-06-07 20:22:41 +0900

    비밀번호변경 잘못 올려서 다시 커밋

--- src/main/java/com/example/newspeed/service/ProfileService.java
+++ src/main/java/com/example/newspeed/service/ProfileService.java
@@ -3,25 +3,23 @@ package com.example.newspeed.service;
 import com.example.newspeed.dto.ProfileRequestDto;
 import com.example.newspeed.dto.ProfileResponseDto;
 import com.example.newspeed.entity.User;
-import com.example.newspeed.repository.ProfileRepository;
 import com.example.newspeed.repository.UserRepository;
 import com.example.newspeed.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class ProfileService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public ProfileService(UserRepository userRepository) {
+    public ProfileService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ProfileResponseDto getProfile(@PathVariable(name = "id") long id) {
@@ -38,16 +36,16 @@ public class ProfileService {
 
     public ProfileResponseDto updatePassword(UserDetailsImpl userDetails, ProfileRequestDto requestDto) {
         User user = userDetails.getUser();
-        if (!user.getPassword().equals(requestDto.getPassword())) {
+        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
         }
 
-        if(user.getPassword().equals(requestDto.getNewPassword())){
+        if(passwordEncoder.matches(requestDto.getNewPassword(), user.getPassword())){
             throw new IllegalArgumentException("이전과 같은 비밀번호입니다.");
         }
 
 
-        user.updatePassword(requestDto.getNewPassword());
+        user.updatePassword(passwordEncoder.encode(requestDto.getNewPassword()));
         userRepository.save(user);
         return ProfileResponseDto.toDto(user);
     }
